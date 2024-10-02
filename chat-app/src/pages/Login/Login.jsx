0e5e148b165b66ce5a83523338d0190d7d9ffb63
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-03 00:18:55 +0800

    Main Page
    Main Page with css

--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState("Sign Up");
+
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className="logo" />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input type="text" placeholder='username' className="form-input" required/>
+        <h2>{currState}</h2>
+        {currState === "Sign Up"? <input type="text" placeholder='username' className="form-input" required/> :null}
         <input type="email" placeholder='Email adress' className="form-input" required/>
         <input type="password" placeholder='password' className="form-input" required/>
         <button type='submit'>Sign Up</button>
@@ -17,7 +20,7 @@ const Login = () => {
           <p>I agree to the <span>terms of use & privacy policy.</span></p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account? <span>Click Here</span></p>
+          <p className="login-toggle">Already have an account? <span onClick={()=> setCurrState("Login")}>Click Here</span></p>
         </div>
       </form>
     </div>
