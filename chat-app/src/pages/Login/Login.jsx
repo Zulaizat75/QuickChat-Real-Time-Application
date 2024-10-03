ecc14e88c624857c8ccaba3e630badd3fe41d14c
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-03 20:28:58 +0800

    chatapp
    Login Page UI

--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -14,13 +14,17 @@ const Login = () => {
         {currState === "Sign Up"? <input type="text" placeholder='username' className="form-input" required/> :null}
         <input type="email" placeholder='Email adress' className="form-input" required/>
         <input type="password" placeholder='password' className="form-input" required/>
-        <button type='submit'>Sign Up</button>
+        <button type='submit'>{currState === "Sign Up"?"Create Account":"Login Now"}</button>
         <div className="login-term">
           <input type="checkbox" />
           <p>I agree to the <span>terms of use & privacy policy.</span></p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account? <span onClick={()=> setCurrState("Login")}>Click Here</span></p>
+          {
+            currState === "Sign Up"
+            ? <p className="login-toggle">Already have an account? <span onClick={()=> setCurrState("Login")}>Login Here</span></p>
+            : <p className="login-toggle">Create an account <span onClick={()=> setCurrState("Sign Up")}> Click Here</span></p>
+          }
         </div>
       </form>
     </div>
