96562a56176600fc26c9a3d3cbb0d64bc71ce373
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-07 22:38:45 +0800

    chatbox ui
    done ui chatbox.jsx

--- chat-app/src/components/LeftSidebar/LeftSidebar.jsx
+++ chat-app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -18,13 +18,16 @@ const LeftSidebar = () => {
             <input type="text" placeholder='Search Here..'/>
         </div>
         <div className="ls-list">
-            <div className="friends">
-                <img src={assets.profile_img} alt="" />
-                <div>
-                    <p>Richard Sanford</p>
-                    <span>Hello, How are you?</span>
+            {Array(12).fill("").map((item,index)=>(
+                <div className="friends">
+                    <img src={assets.profile_img} alt="" />
+                    <div>
+                        <p>Richard Sanford</p>
+                        <span>Hello, How are you?</span>
+                    </div>
                 </div>
-            </div>
+            ))}
+
         </div>
     </div>
   )
