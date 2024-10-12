73376e0087f9ba075a825d61ca82c0b18c1a7a87
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-12 14:15:25 +0800

    Update LeftSidebar.jsx
    searchbar function WIP

--- chat-app/src/components/LeftSidebar/LeftSidebar.jsx
+++ chat-app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,24 +1,82 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import { AppContext } from "../../context/AppContext";
 
 const LeftSidebar = () => {
 
   const navigate = useNavigate();
+  const {userData, chatData} = useContext(AppContext);
+  const [user,setUser] = useState(null);
+  const [showSearch,setShowSearch] = useState(false);
 
   const inputHandler = async (e) => {
     try {
       const input= e.target.value;
-      const userRef = collection(db, 'users');
-      const q = query(userRef,where("username","==",input.toLowerCase()));
-      const querySnap = await getDocs(q);
-      if (!querySnap.empty)
-        {
-          console.log(querySnap.docs[0].data());
-        }
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, 'users');
+        const q = query(userRef,where("username","==",input.toLowerCase()));
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id)
+          {
+            let userExist = false;
+            chatData.map((user)=>{
+              if (user.rId === querySnap.docs[0].data().id) {
+                userExist = true;
+              }
+            })
+            if (!userExist) {
+              setUser(querySnap.docs[0].data());
+            }
+          }
+          else {
+            setUser(null);
+          }
+      }
+      else {
+        setShowSearch(false);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
+    }
+  }
+
+  const addChat = async () => {
+    const messagesRef = collection(db, "messages");
+    const chatsRef = collection(db,"chats");
+    try {
+      const newMessageRef = doc(messagesRef);
+
+      await setDoc(newMessageRef,{
+        createAt:serverTimestamp(),
+        messages:[]
+      })
+
+      await updateDoc(doc(chatsRef,user.id),{
+        chatsData:arrayUnion({
+          messageId:newMessageRef.id,
+          lastMessage:"",
+          rId:userData.id,
+          updatedAt:Date.now(),
+          messageSeen:true
+        })
+      })
+
+      await updateDoc(doc(chatsRef,userData.id),{
+        chatsData:arrayUnion({
+          messageId:newMessageRef.id,
+          lastMessage:"",
+          rId:user.id,
+          updatedAt:Date.now(),
+          messageSeen:true
+        })
+      })
+
     } catch (error) {
       
     }
@@ -44,17 +102,23 @@ const LeftSidebar = () => {
         <input onChange={inputHandler} type="text" placeholder="Search Here.." />
       </div>
       <div className="ls-list">
-        {Array(12)
-          .fill("")
-          .map((item, index) => (
-            <div className="friends" key={index}> 
-              <img src={assets.profile_img} alt="" />
-              <div>
-                <p>Richard Sanford</p>
-                <span>Hello, How are you?</span>
-              </div>
+        {showSearch && user
+        ? <div onClick={addChat} className="friends add-user">
+          <img src={user.avatar} alt="" />
+          <p>{user.name}</p>
+        </div>
+        :Array(12)
+        .fill("")
+        .map((item, index) => (
+          <div className="friends" key={index}> 
+            <img src={assets.profile_img} alt="" />
+            <div>
+              <p>Richard Sanford</p>
+              <span>Hello, How are you?</span>
             </div>
-          ))}
+          </div>
+        ))
+        }
       </div>
     </div>
   );
