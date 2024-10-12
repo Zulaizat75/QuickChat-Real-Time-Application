667bf88c3f71aec22c7ee952baf2214011691c23
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-12 23:42:13 +0800

    chattapp done backend
    done backend
    WIP integration
    WIP media query
    WIP uiux

--- chat-app/src/components/ChatBox/Chatbox.jsx
+++ chat-app/src/components/ChatBox/Chatbox.jsx
@@ -2,79 +2,170 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Chatbox.css";
 import assets from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
-import { doc, onSnapshot } from "firebase/firestore";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../../config/firebase";
+import upload from "../../lib/upload";
+import { toast } from "react-toastify";
 
 const Chatbox = () => {
-  const { userData, messagesId, chatUser, messages, setMessages } =
+  const { userData, messagesId, chatUser, messages, setMessages, chatVisible, setChatVisible } =
     useContext(AppContext);
 
   const [input, setInput] = useState("");
 
-  useEffect(()=>{
+  const sendMessage = async () => {
+    try {
+      if (input && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            text: input,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setInput("");
+  };
+
+  const sendImage = async (e) => {
+    try {
+      
+      const fileUrl = await upload(e.target.files[0]);
+
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = "Image";
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const convertTimestamp = (timestamp) => {
+    let date = timestamp.toDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    if (hour>12) {
+      return hour-12 + ":" + minute + " PM";
+    }
+    else{
+      return hour + ":" + minute + " AM";
+    }
+  }
+
+  useEffect(() => {
     if (messagesId) {
-      const unSub = onSnapshot(doc(db,'messages',messagesId),(res)=>{
+      const unSub = onSnapshot(doc(db, "messages", messagesId), (res) => {
         setMessages(res.data().messages.reverse());
         console.log(res.data().messages.reverse());
-      })
-      return () =>{
+      });
+      return () => {
         unSub();
-      }
+      };
     }
-  },[messagesId])
+  }, [messagesId]);
 
   return chatUser ? (
-    <div className="chat-box">
+    <div className={`chat-box ${chatVisible ? "" : "hidden"}`}>
       <div className="chat-user">
         <img src={chatUser.userData.avatar} alt="" />
         <p>
-          {chatUser.userData.name}
-          <img className="dot" src={assets.green_dot} alt="" />
+          {chatUser.userData.name} {Date.now()-chatUser.userData.lastSeen <= 70000 ?<img className="dot" src={assets.green_dot} alt="" /> : null}
         </p>
         <img src={assets.help_icon} className="help" alt="" />
+        <img onClick={() => setChatVisible(false)} src={assets.arrow_icon} alt="" className="arrow" />
       </div>
 
       <div className="chat-msg">
-        <div className="s-msg">
-          <p className="msg">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Recusandae, necessitatibus.
-          </p>
-          <div>
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
+        {messages.map((msg, index) => (
+          <div key={index} className={msg.sId === userData.id ? "s-msg" : "r-msg"}>
+            {msg["image"]
+            ? <img className="msg-img" src={msg.image} alt="" />
+            : <p className="msg">{msg.text}</p>
+            }
+            <div>
+              <img src={msg.sId === userData.id ? userData.avatar : chatUser.userData.avatar} alt="" />
+              <p>{convertTimestamp(msg.createdAt)}</p>
+            </div>
           </div>
-        </div>
-        <div className="s-msg">
-          <img className="msg-img" src={assets.pic1} alt="" />
-          <div>
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
-        <div className="r-msg">
-          <p className="msg">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Recusandae, necessitatibus.
-          </p>
-          <div>
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
+        ))}
       </div>
 
       <div className="chat-input">
-        <input type="text" placeholder="Send a message" />
-        <input type="file" id="iamge" accept="image/png, image/jpeg" hidden />
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          placeholder="Send a message"
+        />
+        <input onChange={sendImage}type="file" id="image" accept="image/png, image/jpeg" hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
-        <img src={assets.send_button} alt="" />
+        <img onClick={sendMessage} src={assets.send_button} alt="" />
       </div>
     </div>
   ) : (
-    <div className="chat-welcome">
+    <div className={`chat-welcome ${chatVisible ? "" : "hidden"}`}>
       <img src={assets.logo_icon} alt="" />
       <p>Chat anytime, anywhere</p>
     </div>
--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
-import { signup, login } from '../../config/firebase'
+import { signup, login, resetPass } from '../../config/firebase'
 
 const Login = () => {
 
@@ -38,6 +38,9 @@ const Login = () => {
             currState === "Sign Up"
             ? <p className="login-toggle">Already have an account? <span onClick={()=> setCurrState("Login")}>Login Here</span></p>
             : <p className="login-toggle">Create an account <span onClick={()=> setCurrState("Sign Up")}> Click Here</span></p>
+          }
+          {
+            currState === "Login" ? <p className="login-toggle">Forgot Password ?<span onClick={()=> resetPass(email)}> Reset Here</span></p> : null
           }
         </div>
       </form>
