26ae56141417449f193cfe65fa0f8a60c2807d9e
Zulaizat75 <m.zulaizat96@gmail.com>
2024-10-28 23:57:31 +0800

    quickChat LeftSidebar
    fix leftsidebar menu

--- quick-chat/src/components/LeftSidebar/LeftSidebar.jsx
+++ quick-chat/src/components/LeftSidebar/LeftSidebar.jsx
@@ -3,9 +3,18 @@ import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 import {
-  arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where,
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
 } from "firebase/firestore";
-import { db } from "../../config/firebase";
+import { db, logout } from "../../config/firebase";
 import { AppContext } from "../../context/AppContext";
 import { toast } from "react-toastify";
 
@@ -23,34 +32,43 @@ const LeftSidebar = () => {
   } = useContext(AppContext);
   const [user, setUser] = useState(null);
   const [showSearch, setShowSearch] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
 
+  // toggle sub menu function
+  const toggleMenu = () => setMenuVisible((prev) => !prev);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest(".menu")) {
+        setMenuVisible(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  //fetch user data in search
   const inputHandler = async (e) => {
-    try {
-      const input = e.target.value;
-      if (input) {
-        setShowSearch(true);
-        const userRef = collection(db, "users");
-        const q = query(userRef, where("username", "==", input.toLowerCase()));
+    const input = e.target.value;
+    if (input) {
+      setShowSearch(true);
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", input.toLowerCase()));
+      try {
         const querySnap = await getDocs(q);
         if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
-          let userExist = false;
-          chatData.map((user) => {
-            if (user.rId === querySnap.docs[0].data().id) {
-              userExist = true;
-            }
-          });
-          if (!userExist) {
-            setUser(querySnap.docs[0].data());
-          }
+          const userExist = chatData.some(
+            (user) => user.rId === querySnap.docs[0].data().id
+          );
+          if (!userExist) setUser(querySnap.docs[0].data());
         } else {
           setUser(null);
         }
-      } else {
-        setShowSearch(false);
+      } catch (error) {
+        toast.error("Failed to fetch user");
+        console.error(error);
       }
-    } catch (error) {
-      toast.error(error.message);
-      console.error(error);
+    } else {
+      setShowSearch(false);
     }
   };
 
@@ -88,15 +106,15 @@ const LeftSidebar = () => {
       const uSnap = await getDoc(doc(db, "users", user.id));
       const uData = uSnap.data();
       setChat({
-        messagesId:newMessageRef.id,
-        lastMessage:"",
-        rId:userData.id,
-        updatedAt:Date.now(),
-        messageSeen:true,
-        userData:uData
-      })
-      setShowSearch(false)
-      setChatVisible(true)
+        messagesId: newMessageRef.id,
+        lastMessage: "",
+        rId: userData.id,
+        updatedAt: Date.now(),
+        messageSeen: true,
+        userData: uData,
+      });
+      setShowSearch(false);
+      setChatVisible(true);
     } catch (error) {}
   };
 
@@ -118,24 +136,20 @@ const LeftSidebar = () => {
     } catch (error) {
       toast.error(error.message);
     }
-
   };
 
-  useEffect(()=>{
-
+  useEffect(() => {
+    let isMounted = true;
     const updateChatUserData = async () => {
-
       if (chatUser) {
         const userRef = doc(db, "users", chatUser.userData.id);
         const userSnap = await getDoc(userRef);
-        const userData = userSnap.data();
-        setChatUser(prev=>({...prev,userData:userData}));
+        if (isMounted) setChatUser((prev) => ({ ...prev, userData: userSnap.data() }));
       }
-
-    }
+    };
     updateChatUserData();
-
-  },[chatData])
+    return () => {isMounted = false; };
+  }, [chatData, chatUser]);
 
   return (
     <div className={`ls ${chatVisible ? "hidden" : ""}`}>
@@ -143,22 +157,18 @@ const LeftSidebar = () => {
         <div className="ls-nav">
           <img src={assets.logo} className="logo" alt="" />
           <div className="menu">
-            <img src={assets.menu_icon} alt="" />
-            <div className="sub-menu">
+            <img src={assets.menu_icon} alt="menu icon" onClick={toggleMenu} />
+            <div className={`sub-menu ${menuVisible ? "visible" : ""}`}>
               <p onClick={() => navigate("/profile")}>Edit Profile</p>
               <hr />
-              <p>Logout</p>
+              <p onClick={() => logout()}>Logout</p>
             </div>
           </div>
         </div>
       </div>
       <div className="ls-search">
         <img src={assets.search_icon} alt="" />
-        <input
-          onChange={inputHandler}
-          type="text"
-          placeholder="Search Here.."
-        />
+        <input onChange={inputHandler} type="text" placeholder="e.g John Doe" />
       </div>
       <div className="ls-list">
         {showSearch && user ? (
